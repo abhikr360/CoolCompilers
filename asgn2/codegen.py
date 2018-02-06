f8f574c4323f78e68be3f0d55bbe113617d8839e
abhishek763 <theabhishek763@gmail.com>
2018-02-06 19:06:45 +0530

    next use done

--- asgn2/codegen.py
+++ asgn2/codegen.py
@@ -2,7 +2,16 @@ from enum import Enum
 import csv
 import sys
 
+
+# Global DS #
+basic_block_list = []
+SymbolTable={}
+RegisterDescriptor={'HI': 0, 'LO' : 0, 'r0' : 0, 'at' : 0, 'v0' : 0, 'v1' : 0, 'a0' : 0, 'a1' : 0, 'a2' : 0, 'a3' : 0, 't0' : 0, 't1' : 0, 't2' : 0, 't3' : 0, 't4' : 0, 't5' : 0, 't6' : 0, 't7' : 0, 's0' : 0, 's1' : 0, 's2' : 0, 's3' : 0, 's4' : 0, 's5' : 0, 's6' : 0, 's7' : 0, 't8' : 0, 't9' : 0, 'k0' : 0, 'k1' : 0, 's8' : 0, 'ra' : 0}
+addressDescriptor={}
+NextUse={}
+
 class InstrType(Enum):
+	'''Any Instruction in three Address code can be of one of these types'''
 	ASSIGN = 1
 	GOTO = 2
 	IFGOTO = 3
@@ -18,11 +27,13 @@ class InstrType(Enum):
 	PRINT_STRING = 13
 
 class EntryType(Enum):
+	'''Data Type of variable in three address code'''
 	INTEGER = 1
 	VARIABLE = 2
 	STRING = 3
 
 class Operator(Enum):
+	''' Operators available in our three address code'''
 	LESS_THAN = 1
 	GREATER_THAN = 2
 	LESS_THAN_EQUALS = 3
@@ -35,7 +46,10 @@ class Operator(Enum):
 	DIV = 10
 	MOD = 11
 
+
+
 class statement:
+	''' A instruction in three address code'''
 	def __init__(self):
 		self.linenum =  None
 		self.instr_typ = None
@@ -49,9 +63,13 @@ class statement:
 		self.label = None
 
 	def print_stmt(self):
+		'''Print an instruction'''
 		print(self.linenum,self.instr_typ,self.operator,self.in1,self.in1_type,self.in2,self.in2_type,self.out,self.jump_tagret,self.label)
 
+
+
 def set_inputs(row, curr_statement):
+	'''Reads input stores it in a list of classes'''
 	curr_statement.linenum = row[0]
 	#------------------------------------------------
 	assign_list = ["LESS_THAN", "GREATER_THAN", "LESS_THAN_EQUALS", "GREATER_THAN_EQUALS", "EQUALS", "NOT_EQUALS", "ADD", "SUB", "MUL", "DIV", "MOD"]
@@ -62,7 +80,7 @@ def set_inputs(row, curr_statement):
 	else:
 		exec("curr_statement.instr_typ = InstrType.%s" %(row[1]))
 
-	#--------------- For row 2 ----------------------
+	#         --------------- For row 2 ----------------------
 
 	if(curr_statement.instr_typ == InstrType.ASSIGN):
 		if(curr_statement.operator <> None):					#for statements of type a = b + 2
@@ -95,10 +113,6 @@ def set_inputs(row, curr_statement):
 				curr_statement.in1 = temp
 				curr_statement.in1_type = EntryType.VARIABLE
 			
-
-
-
-
 	elif(curr_statement.instr_typ == InstrType.IFGOTO):
 		exec("curr_statement.operator = Operator.%s" %(row[2]))
 		curr_statement.jump_tagret = int(row[5])
@@ -123,15 +137,135 @@ def set_inputs(row, curr_statement):
 	#-------------------row 2 end -------------------
 	#print(curr_statement.linenum, curr_statement.instr_typ, curr_statement.operator,curr_statement.out,curr_statement.in1,curr_statement.in2)
 
+class Scope(Enum):
+	''' Scope of a variable'''
+	GLOBAL = 1
+	LOCAL = 2
+
+
+def lookup_SymbolTable(s):
+	''' Search in symbol table'''
+	if(s in SymbolTable.keys() and s !=''):
+		return SymbolTable[s]
+	else:
+		return None
+
+def insert_SymbolTable(s, symbolTableEntry):
+	'''Insert into symbol table'''
+	if(s==""):
+		print("symbolName Cannot be empty.....Aborting !!")
+		exit()
+	else:
+		SymbolTable[s]=symbolTableEntry
+
+
+class SymTabEntry:
+	'''One entry of symbol table'''
+	def __init__(self, isLive=False, nextUse=0, dataType=EntryType.INTEGER, scope=Scope.GLOBAL):
+		self.isLive=isLive
+		self.nextUse=nextUse
+		self.dataType=dataType
+		self.scope=scope
+
+
+
+class NextUseEntry:
+	"""for each line : for all three variables involved their next use and is lib=ve information"""
+	def __init__(self, in1, in2, out, in1nextuse, in2nextuse, outnextuse, in1islive, in2islive, outislive):
+		self.in1 = in1
+		self.in2 = in2
+		self.out = out
+		self.in1nextuse = in1nextuse
+		self.in2nextuse = in2nextuse
+		self.outnextuse = outnextuse
+		self.in1islive = in1islive
+		self.in2islive = in2islive
+		self.outislive = outislive
+
+
+def construct_NextUse():
+	# print(len(basic_block_list))
+	i=-1
+	for basic_block in basic_block_list:
+		#Flush Symbol table's nextuse islive information
+		i=i+1
+		for x in SymbolTable:
+			SymbolTable[x].isLive = False
+			SymbolTable[x].nextUse = 0
+
+		# if i==1:
+		# 	print(SymbolTable['a'].isLive)
+		# 	print(SymbolTable['a'].nextUse)
+		# 	break
+		for stmt in reversed(basic_block):
+			in1=""
+			in2=""
+			# stmt.print_stmt()
+			if(stmt.in1_type == EntryType.VARIABLE):
+				in1=stmt.in1
+			if(stmt.in2_type == EntryType.VARIABLE):
+				in2=stmt.in2
+			out = stmt.out
+			in1nextuse=0
+			in2nextuse=0
+			outnextuse=0
+			in1islive=False
+			in2islive=False
+			outislive=False
+
+
+
+			ste1 = lookup_SymbolTable(in1)
+			ste2 = lookup_SymbolTable(in2)
+			steo = lookup_SymbolTable(out)
+			print(in1, in2, out)
+			if(out):
+				if(steo):
+					outnextuse=steo.nextUse
+					outislive=steo.isLive
+					steo.nextUse=0
+					steo.isLive=False
+					SymbolTable[out]=steo
+				else:
+					s=SymTabEntry()
+					insert_SymbolTable(out,s)
+			if(in1):
+				if(ste1):
+					in1nextuse=ste1.nextUse
+					in1islive=ste1.isLive
+					ste1.nextUse=stmt.linenum
+					ste1.isLive=True
+					SymbolTable[in1]=ste1
+				else:
+					s=SymTabEntry(True, stmt.linenum)
+					insert_SymbolTable(in1,s)
+			if(in2):
+				if(ste2):
+					in2nextuse=ste2.nextUse
+					in2islive=ste2.isLive
+					ste2.nextUse=stmt.linenum
+					ste2.isLive=True
+					SymbolTable[in2]=ste2
+				else:
+					s=SymTabEntry(True, stmt.linenum)
+					insert_SymbolTable(in2,s)
+			
+
+			nue = NextUseEntry(in1, in2, out, in1nextuse, in2nextuse, outnextuse, in1islive, in2islive, outislive)
+			NextUse[stmt.linenum]=nue
+
+
 
 def main():
 	code = []
 	leaders = [1]
+	numberoflinesinfile=0
 	with open(str(sys.argv[1]), 'rb') as codefile:
 		line_reader = csv.reader(codefile, delimiter = ',')
+		
 		for row in line_reader:
 			curr_statement = statement()
-			#print(row[1])
+			numberoflinesinfile = numberoflinesinfile + 1
 			set_inputs(row, curr_statement)
 			code.append(curr_statement)
 
@@ -144,28 +278,31 @@ def main():
 				leaders.append(int(curr_statement.linenum))
 
 			#------------------------------------------------
-
-	#code = []
-	# s1 = statement(0,InstrType.assign, 1, SymtabEntryType.integer, 2, SymtabEntryType.integer, 3, SymtabEntryType.variable, 4)
-	# s2 = statement(0,InstrType.assign, 1, SymtabEntryType.integer, 2, SymtabEntryType.integer, 3, SymtabEntryType.variable, 4)
-
-	for x in code:
-		x.print_stmt()
+	# for x in code:
+	# 	x.print_stmt()
+	leaders.append(numberoflinesinfile+1)
 	leaders=set(leaders)
 	leaders=list(leaders)
 	leaders.sort()
 
-	basic_block_list = []
+	# print(leaders)
 
 	for i in range(len(leaders)-1):
-		print(leaders[i]-1,leaders[i+1]-1)
+		# print(leaders[i]-1,leaders[i+1]-1)
 		basic_block = code[leaders[i]-1:leaders[i+1]-1]
 		basic_block_list.append(basic_block)
-	
-	for x in basic_block_list:
-		print(x[0].instr_typ)
+	# Basic block prepared----------------------------------------
+
+	for basic_block in basic_block_list:
+		print("-------")
+		for stmt in basic_block:
+			print(stmt.linenum)
+
+	construct_NextUse()
+
+	# print(NextUse.keys())
+	for x in NextUse:
+		print x,NextUse[x].in1, NextUse[x].in2, NextUse[x].out, NextUse[x].in1nextuse, NextUse[x].in2nextuse, NextUse[x].outnextuse, NextUse[x].in1islive, NextUse[x].in2islive, NextUse[x].outislive
 
 if __name__ == '__main__':
 	main()
-
-
--- asgn2/symboltable.py
+++ asgn2/symboltable.py
@@ -1,54 +1,71 @@
 from enum import Enum
-symbolTable={}
+import codegen
+
+SymbolTable={}
+RegisterDescriptor={'HI': 0, 'LO' : 0, 'r0' : 0, 'at' : 0, 'v0' : 0, 'v1' : 0, 'a0' : 0, 'a1' : 0, 'a2' : 0, 'a3' : 0, 't0' : 0, 't1' : 0, 't2' : 0, 't3' : 0, 't4' : 0, 't5' : 0, 't6' : 0, 't7' : 0, 's0' : 0, 's1' : 0, 's2' : 0, 's3' : 0, 's4' : 0, 's5' : 0, 's6' : 0, 's7' : 0, 't8' : 0, 't9' : 0, 'k0' : 0, 'k1' : 0, 's8' : 0, 'ra' : 0}
+addressDescriptor={}
+
 class SymtabEntryType(Enum):
 	INTEGER = 1
 	VARIABLE = 2
 	STRING = 3
 
+class Scope(Enum):
+	GLOBAL = 1
+	LOCAL = 2
 
-class symTabEntry:
-	count=0;
-	@staticmethod
-	def lookup(s):
-		for e in symbolTable.values():
-			if(e.symbolName==s):
-				return e.symTabIdx
 
-	def __init__(self, symbolName="", isLive=False, nextUse=0, dataType=SymtabEntryType.INTEGER, scope=None):
-		self.symTabIdx=symTabEntry.count;
-		symTabEntry.count = symTabEntry.count + 1
-		self.symbolName = symbolName
+def lookup_SymbolTable(s):
+	if(s in SymbolTable.keys()):
+		return SymbolTable[s]
+	else:
+		return None
+
+def insert_SymbolTable(s, symbolTableEntry):
+	if(s==""):
+		print("symbolName Cannot be empty.....Aborting !!")
+		exit()
+	else:
+		SymbolTable[s]=symbolTableEntry
+
+
+class SymTabEntry:
+	def __init__(self, isLive=False, nextUse=0, dataType=SymtabEntryType.INTEGER, scope=Scope.GLOBAL):
 		self.isLive=isLive
 		self.nextUse=nextUse
 		self.dataType=dataType
 		self.scope=scope
 
+NextUse={}
 
-	def insert(self):
-		if(self.symbolName==""):
-			print("symbolName Cannot be empty.....Aborting !!")
-			exit()
-		else:
-			symbolTable[self.symTabIdx]=self
+class NextUseEntry:
+	"""docstring for NextUseEntry"""
+	def __init__(self, in1, in2, out, in1nextuse, in2nextuse, outnextuse, in1islive, in2islive, outislive):
+		self.in1 = in1
+		self.in2 = in2
+		self.out = out
+		self.in1nextuse = in1nextuse
+		self.in2nextuse = in2nextuse
+		self.outnextuse = outnextuse
+		self.in1islive = in1islive
+		self.in2islive = in2islive
+		self.outislive = outislive
 
+def construct_NextUse():
+	s=1
 	
 def main():
-	
 
-	registerDescriptor={'HI': 0, 'LO' : 0, 'r0' : 0, 'at' : 0, 'v0' : 0, 'v1' : 0, 'a0' : 0, 'a1' : 0, 'a2' : 0, 'a3' : 0, 't0' : 0, 't1' : 0, 't2' : 0, 't3' : 0, 't4' : 0, 't5' : 0, 't6' : 0, 't7' : 0, 's0' : 0, 's1' : 0, 's2' : 0, 's3' : 0, 's4' : 0, 's5' : 0, 's6' : 0, 's7' : 0, 't8' : 0, 't9' : 0, 'k0' : 0, 'k1' : 0, 's8' : 0, 'ra' : 0}
-	addressDescriptor={} # Key for this dictionary will be the index in symbol table
+	# s1 = SymTabEntry()
+	# insert_SymbolTable('a1', s1)
+	# print(SymbolTable)
+	# s2 = SymTabEntry()
+	# insert_SymbolTable('a_b', s2)
 
-	s1 = symTabEntry('abf', True, 0)
-	s1.insert()
-	print(symbolTable)
-	s2 = symTabEntry('kl')
+	# print(lookup_SymbolTable('s'))
+	# print(lookup_SymbolTable('a1').isLive)
+	print(codegen.basic_block_list)
 
-	s2.insert()
-	s3 = symTabEntry('hj')
-			
-	s3.insert()
-
-	print(symTabEntry.lookup('kl'))
 
 if __name__ == '__main__':
 	main()
