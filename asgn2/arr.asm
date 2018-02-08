d6554a793320b378dc9dc980da190ce1ef1da285
Tushar Gupta <tushargupta5197@gmail.com>
2018-02-08 16:45:45 +0000

    assm

--- asgn2/arr.asm
+++ asgn2/arr.asm
@@ -1,4 +1,5 @@
 .data
+
 i : .word 0
 arr : .space 40
 sum : .word 0
@@ -8,13 +9,11 @@ temp : .word 0
 
 main:
 
-li $t5, 0
-
-sw t5, $i
+sw $t5, i
 #------------new block started---------------------
 LOOP:
 
-sw t5, $i
+sw $t5, i
 #------------new block started---------------------
 li $t7,10
 bge $t5,$t7,LOOPEXIT
@@ -25,21 +24,21 @@ add $t5, $t5, $t5
 add $t5, $t5, $t5
 add $t5, $t5, $s6
 sw $t5, 0($t5)
-sw t5, $i
-sw s6, $arr
+sw $t5, i
+sw $s6, arr
 #------------new block started---------------------
 li $t7,10
 blt $t5,$t7,LOOP
 
 LOOP_EXIT:
 
-sw t5, $i
+sw $t5, i
 #------------new block started---------------------
 li $t5, 0
 
 SUM_LOOP:
 
-sw t5, $sum
+sw $t5, sum
 #------------new block started---------------------
 li $t7,10
 bge $t5,$t7,SUM_LOOPEXIT
@@ -52,16 +51,21 @@ lw $s5, 0($t5)
 lw $s4, sum
 add $s4, $s4, $s5
 
-sw t5, $i
-sw s6, $arr
-sw s4, $sum
-sw s5, $temp
+sw $t5, i
+sw $s6, arr
+sw $s4, sum
+sw $s5, temp
 #------------new block started---------------------
 li $t7,10
 blt $t5,$t7,SUM_LOOP
 
 SUM_LOOPEXIT:
 
-sw t5, $i
-.endmain
+sw $t5, i
 #------------new block started---------------------
+
+lw $a0, sum
+li $v0, 1
+syscall
+
+.end main
