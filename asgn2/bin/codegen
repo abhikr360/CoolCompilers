2daa41a2c4e510342ae8a0efa553a7049c4be19b
mkant956 <kantmadhu956@gmail.com>
2018-02-11 22:53:52 +0530

    makefile done

--- asgn2/bin/codegen
+++ asgn2/bin/codegen
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from enum import Enum
 import csv
 import sys
--- asgn2/temp.asm
+++ asgn2/temp.asm
@@ -1,68 +1,96 @@
 .data
 space: .asciiz " "
-a : .word 0
-c : .word 0
-e : .word 0
-d : .word 0
-f : .word 0
+i : .word 0
+jaj : .word 0
+array : .space 12
+rev_array : .space 12
+t : .word 0
 .text
 main:
-lw $t5,a
-li $t5, 2
+lw $t5,i
+li $t5, 0
 
-lw $s6,c
-li $s6, 3
+sw $t5, i
 
-sw $t5, a
-sw $s6, c
+#-----------------------------------block id: 140461103215248
+scan:
 
-bge $t5,$s6,L
-
-#-----------------------------------block id: 139684614404720
-lw $t5, a
-lw $s6, c
-mult $t5, $s6
-mflo $t5
-
-sw $t5, a
-sw $s6, c
-
-#-----------------------------------block id: 139684614405008
-L:
-
-lw $t5, c
-lw $s6, a
-sub $t5, $t5, $s6
-
-move $a0,$t5
-jal print_int
-
-lw $s5,d
-li $s5, 1
-
-lw $s4,e
-li $s4, 1
-
-lw $s3, f
+la $t5, array
+lw $s6, i
 jal scan_int
-move $s3,$v0
+sll $t7, $s6, 2
+add $t8, $t5, $t7
+sw $v0,0($t8)
 
-add $s3, $s3, $s5
+addi $s6, $s6, 1
 
-add $s3, $s3, $s4
+li $t7,3
+sw $s6, i
+
+blt $s6,$t7,scan
+
+#-----------------------------------block id: 140461103215464
+lw $t5,i
+li $t5, 0
+
+lw $s6,jaj
+li $s6, 2
+
+sw $t5, i
+sw $s6, jaj
+
+#-----------------------------------block id: 140461103215536
+rev:
+
+la $t5, array
+lw $s6, i
+lw $s5,t
+sll $t7, $s6, 2
+add $t8, $t5, $t7
+lw $s5, 0($t8)
+
+lw $s4, jaj
+la $s3, rev_array
+sll $t7, $s4, 2
+add $t8, $s3, $t7
+sw $s5, 0($t8)
+addi $s6, $s6, 1
+
+addi $s4, $s4, -1
+
+li $t7,3
+sw $s6, i
+sw $s4, jaj
+sw $s5, t
+
+blt $s6,$t7,rev
+
+#-----------------------------------block id: 140461103215608
+lw $t5,i
+li $t5, 0
+
+sw $t5, i
+
+#-----------------------------------block id: 140461103215680
+print:
+
+la $t5, rev_array
+lw $s6, i
+sll $t7, $s6, 2
+add $t8, $t5, $t7
+lw $a0,0($t8)
+jal print_int
 
 jal space_func
 
-move $a0,$s3
-jal print_int
+addi $s6, $s6, 1
 
-sw $s6, a
-sw $t5, c
-sw $s4, e
-sw $s5, d
-sw $s3, f
+li $t7,3
+sw $s6, i
 
-#-----------------------------------block id: 139684614405080
+blt $s6,$t7,print
+
+#-----------------------------------block id: 140461103215752
 exit_func:
 li $v0,10
 syscall
