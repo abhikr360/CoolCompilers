13aab92d2cd3c1376c1134218301eae142d9e73d
abhishek763 <theabhishek763@gmail.com>
2018-04-03 18:41:43 +0530

    types added

--- asgn4/tree.py
+++ asgn4/tree.py
@@ -13,18 +13,22 @@ class ArgumentList:
 
 class FunctionCall:
 	"""docstring for FunctionCall"""
-	def __init__(self, code):
+	def __init__(self, code,datatype='Int'):
 		self.code = code
+		self.datatype = datatype
 
 class Feature:
 	"""docstring for Feature"""
-	def __init__(self, code):
+	def __init__(self, code, datatype='Int'):
 		self.code = code
+		self.datatype = datatype
 
 class FeatureHeader:
 	"""docstring for FeatureHeader"""
-	def __init__(self, code):
+	def __init__(self, code, datatype='Int'):
 		self.code = code
+		self.datatype = datatype
+
 
 class FeatureBody:
 	"""docstring for FeatureBody"""
@@ -41,14 +45,19 @@ class Class:
 	"""docstring for Class"""
 	def __init__(self, code):
 		self.code = code
+
+
 class Classes:
 	"""docstring for Class"""
 	def __init__(self, code):
 		self.code = code
+
+
 class Program:
 	"""docstring for Class"""
 	def __init__(self, code):
 		self.code = code
+
 class ProgramImport:
 	"""docstring for Class"""
 	def __init__(self, code):
@@ -62,35 +71,40 @@ class Start:
 
 class If_Then_Else:
 	"""docstring for Class"""
-	def __init__(self, code):
+	def __init__(self, code,datatype='Int'):
 		self.code = code
+		self.datatype=datatype
 
 
 class While:
 	"""docstring for Class"""
-	def __init__(self, code):
+	def __init__(self, code,datatype='Int'):
 		self.code = code
+		self.datatype=datatype
 
 class For:
 	"""docstring for Class"""
-	def __init__(self, code):
+	def __init__(self, code,datatype='Int'):
 		self.code = code
+		self.datatype=datatype
 
 class BlockExpression:
 	"""docstring for BlockExpression"""
-	def __init__(self, code):
+	def __init__(self, code,datatype='Int'):
 		self.code=code
+		self.datatype=datatype
 
 class BlockList:
 	"""docstring for BlockExpression"""
-	def __init__(self, code):
+	def __init__(self, code,datatype='Int'):
 		self.code=code
+		self.datatype=datatype
 
 class Formal:
 	"""docstring for Formal"""
-	def __init__(self, code):
+	def __init__(self, code, datatype='Int'):
 		self.code = code
-		# self.symtab = symtab
+		self.datatype = datatype
 		
 class FormalParameterList(object):
 	"""docstring for FormalParameterList"""
@@ -101,7 +115,7 @@ class FormalParameterList(object):
 
 class FormalParameter:
 	"""docstring for FormalParameter"""
-	def __init__(self, code, place, datatype):
+	def __init__(self, code, place, datatype='Int'):
 		self.code = code
 		self.place = place
 		self.datatype = datatype
@@ -110,11 +124,18 @@ class FormalParameter:
 
 class Let(object):
 		"""docstring for Let"""
-		def __init__(self, code=[]):
+		def __init__(self, code=[],datatype='Int'):
 			self.code=code
-				
+			self.datatype=datatype
+
+
 class Type(object):
 	"""docstring for Type"""
 	def __init__(self,place):
 		self.place = place
+		
+class ClassHeader(object):
+	"""docstring for ClassHeader"""
+	def __init__(self, code):
+		self.code=code
 		
